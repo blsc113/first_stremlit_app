d1504eba95473703ab988687daf3ed38b4777824
blsc113 <124112693+blsc113@users.noreply.github.com>
2023-01-31 23:39:00 -0300

    Update streamlit_app.py
    Added emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents Healthy Dinner')
+streamlit.title(💕'My Parents Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
