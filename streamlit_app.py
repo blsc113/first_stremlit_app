27d6c1886199b8db931a0407354a295d32db64c1
blsc113 <124112693+blsc113@users.noreply.github.com>
2023-02-12 23:28:36 -0300

    Update streamlit_app.py
    vvv

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,4 @@ if streamlit.button('Add a fruit to the list'):
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
-
-
+    
